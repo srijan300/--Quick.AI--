28d97e14fe038efd84423159658e06899ce0db79
srijan300 <paulsrijan300@gmail.com>
2026-01-18 21:20:53 +0530

    3

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -104,7 +104,7 @@ export const generateArticle = async (req, res)=>{
         }
 
         const response = await AI.chat.completions.create({
-            model: "gemini-2.0-flash",
+            model: "gemini-3-flash-preview",
             messages: [{
                     role: "user",
                     content: prompt,
@@ -154,7 +154,7 @@ export const generateBlogTitle = async (req, res)=>{
         }
 
         const response = await AI.chat.completions.create({
-            model: "gemini-2.0-flash",
+            model: "gemini-3-flash-preview",
             messages: [{ role: "user", content: prompt, } ],
             temperature: 0.7,
             max_tokens: 100,
@@ -324,7 +324,7 @@ export const resumeReview = async (req, res)=>{
         const prompt = `Review the following resume and provide constructive feedback on its strengths, weaknesses, and areas for improvement. Resume Content:\n\n${pdfData.text}`
 
        const response = await AI.chat.completions.create({
-            model: "gemini-2.0-flash",
+            model: "gemini-3-flash-preview",
             messages: [{ role: "user", content: prompt, } ],
             temperature: 0.7,
             max_tokens: 1000,
